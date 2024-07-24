a58336704996520b01262bb6cd83ad1b776108bd
Hossein Hosseinpour <h.hosseinpour75@gmail.com>
2024-07-24 18:48:33 +0330

    Define a function that gets a number and returns the number of Collatz loops

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -25,4 +25,23 @@ export default class Utils {
       }
     }
   }
+
+  static collatzSteps(n: number): number {
+    if (n <= 0) {
+      throw new Error("Input must be a positive integer.");
+    }
+
+    let steps = 0;
+
+    while (n !== 1) {
+      if (n % 2 === 0) {
+        n = n / 2;
+      } else {
+        n = 3 * n + 1;
+      }
+      steps++;
+    }
+
+    return steps;
+  }
 }
