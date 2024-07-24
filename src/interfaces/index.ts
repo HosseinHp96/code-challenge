11d2ebf54cae6390fc005efc6c58437ca3a588c1
Hossein Hosseinpour <h.hosseinpour75@gmail.com>
2024-07-24 11:45:18 +0330

    Create an interface for user state in zustand

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,7 +1,15 @@
+// User interfaces
 export interface IUserEntity {
   name: string;
   userName: string;
   email: string;
   phone: string;
   status: "active" | "not_active" | null;
+}
+
+// Zustand interfaces
+export interface IUsersState {
+  users: IUserEntity[];
+  addUser: (user: IUserEntity) => void;
+  updateUser: (user: IUserEntity) => void;
 }
