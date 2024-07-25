5e118fd64631c09821e1b85b5f36ca2aee4e9c65
Hossein Hosseinpour <h.hosseinpour75@gmail.com>
2024-07-25 18:00:11 +0330

    Create input section for user form

---
+++ src/pages/userFrom/input/index.tsx
@@ -0,0 +1,27 @@
+import { FieldValues, UseFormRegister } from "react-hook-form";
+import { IUserEntity } from "../../../interfaces";
+import { Input } from "./styled";
+import Utils from "../../../utils";
+import { Label, Wrapper } from "../styled";
+
+interface IInputItem<T extends FieldValues> {
+  register: UseFormRegister<T>;
+  name: keyof T;
+  error?: string;
+}
+
+const InputItem: React.FC<IInputItem<IUserEntity>> = (params) => {
+  const { register, error, name } = params;
+
+  return (
+    <Wrapper>
+      <Label htmlFor={name}>{Utils.capitalizeFirstLetter(name)}</Label>
+
+      <Input id={name} {...register(name)} />
+
+      <p>{error}</p>
+    </Wrapper>
+  );
+};
+
+export default InputItem;
---
+++ src/pages/userFrom/input/styled.ts
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+
+export const Input = styled.input`
+  padding: 0.5em;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  font-size: 1rem;
+`;
