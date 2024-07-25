69763f093271dec66691c72540fe91117d063509
Hossein Hosseinpour <h.hosseinpour75@gmail.com>
2024-07-25 17:58:14 +0330

    Create a container

---
+++ src/styles/container.ts
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  margin: 20px;
+  padding: 20px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  background-color: #fcfcfc;
+
+  @media (min-width: 768px) {
+    margin: 40px;
+    padding: 40px;
+  }
+`;
